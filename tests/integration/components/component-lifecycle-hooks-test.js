4d756158b73b2b318f3701733be1b6188877925c
Maan Shamoon <maanshamoon@yahoo.com>
2019-07-14 17:15:54 -0700

    Added component integration tests.

--- tests/integration/components/component-lifecycle-hooks-test.js
+++ tests/integration/components/component-lifecycle-hooks-test.js
@@ -4,23 +4,29 @@ import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | component-lifecycle-hooks', function(hooks) {
-  setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+    setupRenderingTest(hooks);
 
-    await render(hbs`<ComponentLifecycleHooks />`);
+    test('Test component lifecycle function hooks gets triggered.', async function(assert) {
 
-    assert.equal(this.element.textContent.trim(), '');
+        assert.expect(7);
+        
+        this.set('initHook', function() {assert.ok(1);});
+        this.set('willRenderHook', function() {assert.ok(1);});
+        this.set('didInsertElementHook', function() {assert.ok(1);});
+        this.set('didRenderHook', function() {assert.ok(1);});
+        this.set('willDestroyElementHook', function() {assert.ok(1);});
+        this.set('willClearRenderHook', function() {assert.ok(1);});
+        this.set('didDestroyElementHook', function() {assert.ok(1);});
 
-    // Template block usage:
-    await render(hbs`
-      <ComponentLifecycleHooks>
-        template block text
-      </ComponentLifecycleHooks>
-    `);
-
-    assert.equal(this.element.textContent.trim(), 'template block text');
-  });
+        await render(hbs`{{ComponentLifecycleHooks
+            init-hook = (action initHook)
+            will-render-hook = (action willRenderHook)
+            did-insert-element-hook = (action didInsertElementHook)
+            did-render-hook = (action didRenderHook)
+            will-destroy-element-hook = (action willDestroyElementHook)
+            will-clear-render-hook = (action willClearRenderHook)
+            did-destroy-element-hook = (action didDestroyElementHook)
+        }}`);
+    });
 });
